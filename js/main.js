02b13c4be3a9294580238b0841f38c3df5efdeac
Pikkarav <valtteri.pikkarainen@metropolia.fi>
2024-05-10 21:43:33 +0300

    Lisäsin async function.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-function main(x,y){
-    return x+y;
+
+
+async function getapi() {
+    try {
+        // Fetch data using the Fetch API
+        let response = await fetch('http://127.0.0.1:3000/maat/kaupungit/koordinaatit');
+
+        // Check if response is ok
+        if (!response.ok) {
+            throw new Error('Failed to fetch joke');
+        }
+        let jsonData = await response.json();
+
+        console.log(jsonData);
+        return(jsonData)
+
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-
+getapi();
